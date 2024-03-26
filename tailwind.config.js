4f67749f22d20cd3e0cfba0442de8a8888866269
H-ymt <y.handai1272@gmail.com>
2024-03-26 09:46:03 +0900

    ci(prettier): Format by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("tailwindcss-animated")],
+  plugins: [require('daisyui'), require('tailwindcss-animated')],
   daisyui: {
-    themes: ["corporate"],
+    themes: ['corporate'],
   },
-}
+};
