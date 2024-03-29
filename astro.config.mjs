9373dd1f776d2817fda0bc463e8305ef91617822
H-ymt <y.handai1272@gmail.com>
2024-03-29 14:00:59 +0900

    feat(site url): Add Site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://astro-template-daisyui.vercel.app/',
   integrations: [tailwind(), icon()],
 });
